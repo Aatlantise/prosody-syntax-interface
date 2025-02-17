f91e3a0f55f176352eab819486d8b38444890078
Aatlantise <jmin10@jhu.edu>
2025-02-17 17:12:00 -0500

    [Minor feature] Add env.sh

--- env.sh
+++ env.sh
@@ -2,5 +2,7 @@
 module load anaconda3/3.11
 module load cuda/12.5
 nvcc --version
-source ../prosody/bin/activate
+source ../.virtualenv/prosody/bin/activate
 python3 -V
+
+# needs to be run with source, not bash
